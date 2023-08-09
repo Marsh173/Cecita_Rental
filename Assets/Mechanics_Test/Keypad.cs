fc070ab14865988f5281da5490a0b6a6a7ad4bfd
Qifan Zhang <30319124+Marsh173@users.noreply.github.com>
2023-08-09 23:42:49 +0800

    keypad working

---
+++ Assets/Mechanics_Test/Keypad.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Keypad : MonoBehaviour
+{
+    [SerializeField] private TMP_Text Ans;
+    public string Answer = "1234";
+    [SerializeField] private Animator door;
+    [SerializeField] private TMP_Text Ans3d;
+    
+
+    public void Number(int number)
+    {
+        if(Ans.text.Length < 4)
+        {
+            Ans.text += number.ToString();
+            Ans3d.text = Ans.text;
+        }
+        
+    }
+
+    public void Execute()
+    {
+        if(Ans.text == Answer)
+        {
+            //Open Door
+            Ans.text = "PASS";
+            Ans.color = Color.green;
+            Ans3d.text = "PASS";
+            Ans3d.color = Color.green;
+            door.SetBool("Open", true);
+        }
+        else
+        {
+            Ans.text = "ERROR";
+            Ans.color = Color.red;
+            Ans3d.text = "ERROR";
+            Ans3d.color = Color.red;
+        }
+    }
+
+    public void Clear()
+    {
+        Ans.text = "";
+        Ans.color = Color.black;
+
+        Ans3d.text = "";
+        Ans3d.color = Color.black;
+    }
+
+   
+    
+}

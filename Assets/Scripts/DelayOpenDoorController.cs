51a818c7b67ef1575109e971585a7478c16c81ba
Qifan Zhang <30319124+Marsh173@users.noreply.github.com>
2023-09-19 00:34:47 -0400

    door open and close

---
+++ Assets/Scripts/DelayOpenDoorController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayOpenDoorController : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor = null;
+    private bool OpenDoorNow = false;
+
+    [SerializeField] private string doorOpen = "DoorOpen";
+    
+
+    [SerializeField] private float delay = 0.0f;
+
+    private void Update()
+    {
+        if (!OpenDoorNow)
+        {
+            Debug.Log("to delay");
+            StartCoroutine(delayOpen());
+        }
+    
+    }
+
+    IEnumerator delayOpen()
+    {
+        OpenDoorNow = true;
+        yield return new WaitForSeconds(delay);
+        Debug.Log("finish delay");
+        myDoor.Play(doorOpen, 0, 0.0f);
+       
+    }
+
+}

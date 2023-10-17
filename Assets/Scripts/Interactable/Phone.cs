d42ca0c62f4ae1fab0e704ad16d72ca180807955
ZericZhu <zeric1119@yahoo.com>
2023-10-17 14:56:52 -0400

    Phone Puzzle Finished

--- Assets/Scripts/Interactable/Phone.cs
+++ Assets/Scripts/Interactable/Phone.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using TMPro;
 
 public class Phone : MonoBehaviour
 {
     public RawImage PhoneCam;
     public bool isInCam;
+    public GameObject PhoneUI;
+    public string phoneNumber;
+    public TMP_Text PhoneNumberText;
+    public string correctNumber;
+    private bool CanDial;
     public void PhoneInteract()
     {
         PhoneCam.enabled = true;
@@ -16,6 +22,8 @@ public class Phone : MonoBehaviour
         FirstPersonAIO.instance.playerCanMove = false;
         isInCam = true;
         Cursor.visible = true;
+        PhoneUI.SetActive(true);
+        CanDial = true;
     }
 
     private void Update()
@@ -28,8 +36,52 @@ public class Phone : MonoBehaviour
                 PhoneCam.DOFade(0, 1.2f);
                 FirstPersonAIO.instance.enableCameraMovement = true;
                 FirstPersonAIO.instance.playerCanMove = true;
-
+                Cursor.visible = false;
+                PhoneUI.SetActive(false);
             }
         }
     }
+
+    public void AddNumber(float temp)
+    {
+        if (phoneNumber.Length <10 && CanDial)
+        {
+            phoneNumber += temp;
+            PhoneNumberText.text = phoneNumber;
+        }
+
+    }
+
+    public void ClearNumber()
+    {
+        if (CanDial)
+        {
+            phoneNumber = "";
+            PhoneNumberText.text = phoneNumber;
+        }
+    }
+
+    public void CallNumber()
+    {
+        if (phoneNumber == correctNumber)
+        {
+            PhoneNumberText.text = "dialing...";
+        }
+        else
+        {
+            StartCoroutine(WrongNumber());
+            ClearNumber();
+        }
+    }
+
+    IEnumerator WrongNumber()
+    {
+        PhoneNumberText.text = "Wrong!";
+        CanDial = false;
+        yield return new WaitForSeconds(1);
+        phoneNumber = "";
+        PhoneNumberText.text = phoneNumber;
+        CanDial = true;
+    }
+
 }

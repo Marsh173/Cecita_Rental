fb7f62bb2c01290b4cc853ce5240aa59a678c5b1
Teresa Bing <teresabing0513lk@gmail.com>
2023-10-27 07:45:54 -0400

    recharge bar final update

--- Assets/Scripts/Eye_Scripts/NoSightAllowed.cs
+++ Assets/Scripts/Eye_Scripts/NoSightAllowed.cs
@@ -10,8 +10,7 @@ public class NoSightAllowed : MonoBehaviour
     public static NoSightAllowed instance;
     private Button eye_UI;
     public Sprite Eye_Open, Eye_Close;
-    public GameObject SoundCollecting, itemAdded, F;
-    public KeyCode keyF;
+    public GameObject SoundCollecting, itemAdded, FInstruction;
     public Animator anim;
 
     //timer
@@ -25,7 +24,8 @@ public class NoSightAllowed : MonoBehaviour
     private float CurrentEyeBarAmount;
     public GameObject ChargeBarUI;
     public Image RedAura, BarImage;
-
+    private WaitForSeconds rechargeTick = new WaitForSeconds(0.001f);
+    private Coroutine rechargeC;
     private void Start()
     {
         Respawn.restarted = false;
@@ -60,27 +60,29 @@ public class NoSightAllowed : MonoBehaviour
         RedAura.color = new Color(RedAura.color.r, RedAura.color.g, RedAura.color.b, 0);
     }
 
-    private void Update()
+    private void FixedUpdate()
     { 
-        //Key binding, activate animation
-        if(Input.GetKeyDown(keyF))
+        if(Input.GetKeyDown(KeyCode.F))
         {
-            FadeToColor(eye_UI.colors.pressedColor);
-            eye_UI.onClick.Invoke();
-            F.GetComponent<TMP_Text>().text = F.GetComponent<TMP_Text>().text == "Press F to open  your eyes" ? "Press F to close your eyes" : "Press F to open  your eyes";
+            ChangeSprite();
+            //eye_UI.onClick.Invoke();
+            FInstruction.GetComponent<TMP_Text>().text = FInstruction.GetComponent<TMP_Text>().text == "Press F to open your eyes" ? "Press F to close your eyes" : "Press F to open your eyes";
 
-            //eye open
-            if (F.GetComponent<TMP_Text>().text == "Press F to close your eyes")
+            //eye close recharge bar
+            if (FInstruction.GetComponent<TMP_Text>().text == "Press F to open your eyes")
             {
+                if(rechargeC != null)
+                {
+                    StopCoroutine(rechargeC);
+                }
+
+                rechargeC = StartCoroutine(RechargeBar());
+                RedAura.color = new Color(RedAura.color.r, RedAura.color.g, RedAura.color.b, 0);
             }
             else
             {
-                RedAura.color = new Color(RedAura.color.r, RedAura.color.g, RedAura.color.b, 0);
+                StopCoroutine(rechargeC);
             }
-        }
-        else if(Input.GetKeyUp(keyF))
-        {
-            FadeToColor(eye_UI.colors.normalColor);
         }
 
         //Timer decrease
@@ -96,7 +98,7 @@ public class NoSightAllowed : MonoBehaviour
             }
 
             countDown -= 1 * Time.deltaTime;
-            countdownText.text = countDown.ToString("0");
+            //countdownText.text = countDown.ToString("0");
 
             if(countDown <= 3f)
             {
@@ -104,60 +106,57 @@ public class NoSightAllowed : MonoBehaviour
             }
 
             //death when timer's out
-            if (countDown <= 0)
+            /*if (countDown <= 0)
             {
                 Respawn.dead = true;
                 countdownText.text = "";
-            }
+            }*/
         }
         else
         {
             countDown = countDownTime;
-
-            //StartCoroutine(RechargeBar());
-            if (CurrentEyeBarAmount < 100)
-            {
-                CurrentEyeBarAmount = Mathf.SmoothDamp(BarImage.fillAmount * 100f, 100f, ref RechargeSpeed, 500 * Time.deltaTime);
-                BarImage.fillAmount = CurrentEyeBarAmount / 100f;
-            }
         }
 
-        if (F.GetComponent<TMP_Text>().text == "Press F to close your eyes")
+        //recharge bar
+        if (FInstruction.GetComponent<TMP_Text>().text == "Press F to close your eyes")
         {
-            CurrentEyeBarAmount = Mathf.SmoothDamp(BarImage.fillAmount * 100, 0, ref EyeBarDecreaseSpeed, 500 * Time.deltaTime);
-            Debug.Log(CurrentEyeBarAmount);
-            BarImage.fillAmount = CurrentEyeBarAmount / 100f;
+            //decrease when eye open
+            if(TimerActive && CurrentEyeBarAmount != 0)
+            {
+                CurrentEyeBarAmount -= 100/countDownTime * Time.deltaTime;
+                Debug.Log("speed" + 100 / countDownTime * Time.deltaTime);
+
+            }
             
             if (CurrentEyeBarAmount < 0)
             {
                 Respawn.dead = true;
             }
         }
-        /*else
-        {
-            RedAura.color = new Color(RedAura.color.r, RedAura.color.g, RedAura.color.b, 0);
-        }*/
-
+        BarImage.fillAmount = CurrentEyeBarAmount / 100f;
     }
 
     //Animation
     public void ChangeSprite()
     {
-        //
+        //close eye animation
         if (eye_UI.image.sprite == Eye_Open)
         {
             RedAura.color = new Color(RedAura.color.r, RedAura.color.g, RedAura.color.b, 0);
-            eye_UI.image.sprite = Eye_Close; //close
+            FInstruction.GetComponent<TMP_Text>().text = "Press F to close your eyes";
+            eye_UI.image.sprite = Eye_Close;
             anim.SetBool("isBegun", true);
             anim.SetBool("isOpen", false);
             TimerActive = false;
             countdownText.enabled = false;
 
         }
+        //open eye animation
         else
         {
-            eye_UI.image.sprite = Eye_Open; //open
-            anim.SetBool("isBegun", false);
+            eye_UI.image.sprite = Eye_Open;
+            FInstruction.GetComponent<TMP_Text>().text = "Press F to open your eyes";
+            //anim.SetBool("isBegun", false);
             anim.SetBool("isOpen", true);
             TimerActive = true;
             countdownText.enabled = true;
@@ -173,7 +172,14 @@ public class NoSightAllowed : MonoBehaviour
 
     private IEnumerator RechargeBar()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1.5f);
+        Debug.Log("waited");
+        while (CurrentEyeBarAmount < 100)
+        {
+            CurrentEyeBarAmount += RechargeSpeed * Time.deltaTime;
+            yield return rechargeTick;
+        }
 
+        rechargeC = null;
     }
 }

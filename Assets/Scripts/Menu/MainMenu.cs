17567cac61aab7b5e8783e2ce936a595d784a552
balloon223 <sl6879@nyu.edu>
2023-11-01 15:14:15 -0400

    Fixed bug in: MainMenu.cs & FMODStudioChangeMusic.cs
    Fixed the bug where some sounds aren't released properly on scene unload, causing unnecessary sound replays after a game restart.

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -3,40 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+namespace FMODUnity
 {
-    public static bool Setting;
-    //public GameObject settingMenu;
-
-    public string sceneName;
-    
-
-    public void ExitButton()
+    public class MainMenu : MonoBehaviour
     {
-        Application.Quit();
-        Debug.Log("Game Closed");
-    }
+        public static bool Setting;
+        //public GameObject settingMenu;
+        FMOD.ChannelGroup mcg;
+        FMOD.Studio.Bus Masterbus;
 
-    public void LoadScene()
-    {
-        SceneManager.LoadScene(sceneName);
-    }
+        public string sceneName;
 
-    //public void BackButton()
-    //{
-    //    SceneManager.LoadScene("Start_Screen");
-    //}
-    /*
-    public void SettingButton()
-    {
-        Setting = true;
-        settingMenu.SetActive(true);
-    }
 
-    public void BackButton()
-    {
-        Setting = false;
-        settingMenu.SetActive(false);
+        public void ExitButton()
+        {
+            Application.Quit();
+            Debug.Log("Game Closed");
+        }
+
+        public void LoadScene()
+        {
+            FMODUnity.RuntimeManager.CoreSystem.getMasterChannelGroup(out mcg);
+            mcg.stop();
+            SceneManager.LoadScene(sceneName);
+        }
+
+        //public void BackButton()
+        //{
+        //    SceneManager.LoadScene("Start_Screen");
+        //}
+        /*
+        public void SettingButton()
+        {
+            Setting = true;
+            settingMenu.SetActive(true);
+        }
+
+        public void BackButton()
+        {
+            Setting = false;
+            settingMenu.SetActive(false);
+        }
+        */
     }
-    */
 }

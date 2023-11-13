2f159a2e32bc9837142c0ba1e00ff0fca974a341
Qifan Zhang <30319124+Marsh173@users.noreply.github.com>
2023-11-12 21:32:47 -0500

    Day Settup

--- Assets/InspectionCameraTransition.cs
+++ Assets/InspectionCameraTransition.cs
@@ -9,14 +9,48 @@ public class InspectionCameraTransition : MonoBehaviour
     private Camera playercam;
     public Transform InspectionCam;
 
+    private Transform originalCamPosition;
+    private bool isInCam;
+
+    public GameObject inspectionButton; //for adding transcript overlay
+    public GameObject inspectionWindow;
+
+    private void Start()
+    {
+        playercam = FirstPersonAIO.instance.gameObject.GetComponentInChildren<Camera>();
+        originalCamPosition = playercam.transform;
+        inspectionButton.SetActive(false);
+
+    }
+
+    private void Update()
+    {
+        if (isInCam)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Debug.Log("esc");
+                playercam.transform.DOMove(originalCamPosition.position, 1);
+                playercam.transform.DORotate(originalCamPosition.rotation.eulerAngles, 1);
+                FirstPersonAIO.instance.enableCameraMovement = true;
+                FirstPersonAIO.instance.playerCanMove = true;
+                Cursor.visible = false;
+                isInCam = false;
+                inspectionButton.SetActive(false);
+                inspectionWindow.SetActive(false);
+            }
+        }
+    }
 
     public void TransitCamToInspectionPos()
     {
-        playercam = FirstPersonAIO.instance.gameObject.GetComponentInChildren<Camera>();
+        
         playercam.transform.DOMove(InspectionCam.position, 1);
         playercam.transform.DORotate(InspectionCam.rotation.eulerAngles, 1);
         FirstPersonAIO.instance.enableCameraMovement = false;
         FirstPersonAIO.instance.playerCanMove = false;
         Cursor.visible = true;
+        isInCam = true;
+        inspectionButton.SetActive(true);
     }
 }

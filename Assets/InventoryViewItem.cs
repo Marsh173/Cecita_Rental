56c40f7e9e578d6c108700488bfb0b79485914d5
Fatcat1015 <>
2023-11-13 22:53:52 -0500

    item inspection

---
+++ Assets/InventoryViewItem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+//[ExecuteInEditMode]
+public class InventoryViewItem : MonoBehaviour
+{
+    //if there's an object, set the view item active and pull the model
+    [SerializeField] TextMeshProUGUI iconname;
+    [SerializeField] GameObject vig;
+
+    private void Awake()
+    {
+        vig = GameObject.FindGameObjectWithTag("ViewItemGraphic");
+    }
+    public void ViewItem()
+    {
+        if(iconname.text != "")
+        {
+            vig.transform.GetChild(0).gameObject.SetActive(true);
+            vig.transform.GetChild(0).GetChild(2).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = iconname.text;
+        }
+    }
+}

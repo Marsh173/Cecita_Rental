76e781a68578db69260d161ea579a2571dc0547c
Fatcat1015 <>
2023-12-04 16:24:07 -0500

    task manager

--- Assets/Scripts/New_Task/TaskData.cs
+++ Assets/Scripts/New_Task/TaskData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class TaskData :  MonoBehaviour
 {
@@ -10,22 +11,44 @@ public class TaskData :  MonoBehaviour
 
     public TextMeshProUGUI task_title_UI;
     public TextMeshProUGUI task_name_UI;
-
     
     public List<GameObject> subtask = new List<GameObject>();
     public GameObject subtaskPrefab;
     public bool isCompleted;
 
-    public void addSub()
+    public float y_height;
+
+    //add sub and give them a name
+    public void addSub(string subtask_name)
     {
-        var s = Instantiate(subtaskPrefab);
+        var s = Instantiate(subtaskPrefab, gameObject.transform);
         s.transform.SetParent(gameObject.transform);
         subtask.Add(s);
+        s.GetComponent<Sub_Task>().sub_taskName = subtask_name;
+        s.GetComponent<RectTransform>().anchoredPosition = new Vector3(0,-75,0);
     }
 
     private void Update()
     {
         task_title_UI.text = taskTitle;
         task_name_UI.text = taskName;
+
+        if (isCompleted)
+        {
+            FinishTask();
+        }
+    }
+
+    public void FinishTask()
+    {
+        //finish all sub tasks
+
+        //finish main task
+        Destroy(gameObject);
+    }
+
+    public void updateHeight()
+    {
+
     }
 }
--- Assets/Scripts/Sub_Task.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-public class Sub_Task : MonoBehaviour
-{
-    public string sub_taskName;
-    public bool isCompleted;
-}

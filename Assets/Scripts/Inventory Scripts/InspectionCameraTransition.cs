f79ca421dd6a613c027ec952f9f8ed4da907e51c
balloon223 <sl6879@nyu.edu>
2023-12-10 01:57:04 -0500

    FOV change on item interaction (pause menu still needs fixing)

--- Assets/Scripts/Inventory Scripts/InspectionCameraTransition.cs
+++ Assets/Scripts/Inventory Scripts/InspectionCameraTransition.cs
@@ -11,8 +11,9 @@ public class InspectionCameraTransition : MonoBehaviour
     public Transform InspectionCam;
 
     public float initialFOV;
+    public float FOVOnInteraction;
 
-    private Transform originalCamPosition;
+    public Transform originalCamPosition;
     private Vector3 originalPos, originalAngle;
     public bool isInCam;
 
@@ -23,12 +24,12 @@ public class InspectionCameraTransition : MonoBehaviour
 
     private void Start()
     {
-        initialFOV = playercam.fieldOfView;
         instance = this;
         playercam = FirstPersonAIO.instance.gameObject.GetComponentInChildren<Camera>();
         originalCamPosition = playercam.transform;
         inspectionButton.SetActive(false);
         PlayBody = FirstPersonAIO.instance.transform.GetChild(1).gameObject;
+        initialFOV = playercam.fieldOfView;
     }
 
     private void Update()
@@ -50,9 +51,10 @@ public class InspectionCameraTransition : MonoBehaviour
         //playercam.transform.rotation = originalCamPosition.rotation;
         //StartCoroutine(LerpPosition(playercam.transform, originalPos, 1f));
         //StartCoroutine(LerpFunction(playercam.transform, originalAngle, 1f));
+        FirstPersonAIO.instance.enableFOVShift = true;
         playercam.transform.DOMove(originalPos, 1f);
         playercam.transform.DORotate(originalAngle, 1f).OnComplete(() => EnableMovement());
-        //playercam.fieldOfView = initialFOV;
+        playercam.fieldOfView = initialFOV;
 
         gameObject.layer = 7;
     }
@@ -69,9 +71,10 @@ public class InspectionCameraTransition : MonoBehaviour
         Debug.Log(playercam.transform.position + "player pos");
         Debug.Log(InspectionCam.position + "cam pos");
 
+        FirstPersonAIO.instance.enableFOVShift = false;
         playercam.transform.DOMove(InspectionCam.position, 1);
         playercam.transform.DORotate(InspectionCam.rotation.eulerAngles, 1);
-        //playercam.fieldOfView = 20;
+        playercam.fieldOfView = FOVOnInteraction;
 
         FirstPersonAIO.instance.enableCameraMovement = false;
         FirstPersonAIO.instance.playerCanMove = false;

652293cec837e751037a8f8fb95531b7f4b80246
Teresa Bing <teresabing0513lk@gmail.com>
2024-02-04 15:34:29 -0500

    idk

--- Assets/Scripts/temp.cs
+++ Assets/Scripts/temp.cs
@@ -7,21 +7,32 @@ using TMPro;
 public class temp : MonoBehaviour
 {
     public TMP_Text monologue;
-    private void OnTriggerEnter(Collider other)
+
+   
+    private void EnterCutScene()
     {
-        if (other.CompareTag("Player")/* && CutSceneScript.talkedtoNPC*/)
+        foreach (Scene scene in ElevatorController.loadedScenes)
         {
-            CutSceneScript.enteredEndSequen = true;
-            monologue.text = "You hear the delivery being made to each apartment who has a package. " +
-                "Suddenly, a wave of dizziness overflows you, it is as if your body is not your own anymore. You stumble toward the bed room and fell to your knees. " +
-                "Something is definitly not right.";
-            StartCoroutine(wait());
-            SceneManager.LoadScene("Night_One");
+            if (scene.name != "Day_General_System" && scene.name != "Elevator")
+            {
+                SceneManager.UnloadSceneAsync(scene);
+                Debug.Log("Unloaded scene: " + scene.name);
+            }
         }
+
+        SceneManager.LoadScene("Third_Floor", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Protagonist_Room", LoadSceneMode.Additive);
+        StartCoroutine(End());
     }
 
-    IEnumerator wait()
+    IEnumerator End()
     {
+        CutSceneScript.enteredEndSequen = true;
+        monologue.text = "You hear the delivery being made to each apartment who has a package. " +
+            "Suddenly, a wave of dizziness overflows you, it is as if your body is not your own anymore. You stumble toward the bed room and fell to your knees. " +
+            "Something is definitly not right.";
+
         yield return new WaitForSeconds(8f);
+        SceneManager.LoadScene("Night_One");
     }
 }

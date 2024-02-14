8e8eb51bf827b2ec61c4a0f86194784e24673979
Teresa Bing <teresabing0513lk@gmail.com>
2024-02-14 16:00:40 -0500

    fixed inspect sync

--- Assets/Scripts/Inventory Scripts/InventoryViewItem.cs
+++ Assets/Scripts/Inventory Scripts/InventoryViewItem.cs
@@ -7,42 +7,24 @@ using TMPro;
 public class InventoryViewItem : MonoBehaviour
 {
     //if there's an object, set the view item active and pull the model
-    [SerializeField] TMP_Text iconname;
-    [SerializeField, TextArea(3, 10)] string icon_Description = "";
+    public TMP_Text iconname;
+    [TextArea(3, 10)] public string icon_Description = "";
     [SerializeField] int index;
     [SerializeField] GameObject vig;
-
-    public NormalItems NItems;
 
     private void Awake()
     {
         vig = GameObject.FindGameObjectWithTag("ViewItemGraphic");
-        //NItem.displayName;
     }
     public void ViewItem()
     {
         Debug.Log("viewed");
-        Debug.Log("name? " + vig.transform.GetChild(0).GetChild(1).GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text);
-        Debug.Log("name: " + iconname.text + " Des:" + icon_Description);
         
-        //if (iconname != null)
-        //{
-            vig.transform.GetChild(0).gameObject.SetActive(true);
-            vig.GetComponentInChildren<UIInspectCamController>().index_obj = index;
-            vig.GetComponentInChildren<UIInspectCamController>().changeOBJ();
-        /*foreach (var item in NItems)
-        {
-            //find the elemets in each item and replace inventory default
-            GameObject itemobj = Instantiate(NormalItem, NormalItemContent);
-            var itemName = itemobj.transform.Find("itemName").GetComponent<TMP_Text>();
-
-            //display name and image in inventory UI
-            iconname.text = item.displayName;
-            icon_Description = item.descriptions;
-        }*/
-            vig.transform.GetChild(0).GetChild(1).GetChild(1).gameObject.GetComponent<TMP_Text>().text = iconname.text;
-            vig.transform.GetChild(0).GetChild(1).GetChild(1).gameObject.GetComponent<TMP_Text>().text = icon_Description;
+        vig.transform.GetChild(0).gameObject.SetActive(true);
+        vig.GetComponentInChildren<UIInspectCamController>().index_obj = index;
+        vig.GetComponentInChildren<UIInspectCamController>().changeOBJ();
+        
+        vig.transform.GetChild(0).GetChild(1).GetChild(1).gameObject.GetComponent<TMP_Text>().text = iconname.text;
         vig.transform.GetChild(0).GetChild(2).GetChild(0).gameObject.GetComponent<TMP_Text>().text = icon_Description;
-        //}
     }
 }

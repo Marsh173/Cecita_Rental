2e1896ff0f95618e0750f55426175225545be062
Yunfei <1732685232@qq.com>
2024-03-01 11:26:12 -0500

    fix error in hoverinventory

--- Assets/Scripts/Inventory Scripts/HoverInventory.cs
+++ Assets/Scripts/Inventory Scripts/HoverInventory.cs
@@ -2,29 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HoverInventory : MonoBehaviour
 {
     public GameObject hovertext;
-    [SerializeField] private float yPos;
+    [SerializeField] public string transcript;
+    //[SerializeField] private float yPos;
 
     private void Start()
     {
+        hovertext = GameObject.FindGameObjectWithTag("HoverText");
         hidehovertext();
-        yPos = 0;
+        //yPos = 0;
     }
 
     private void Update()
     {
-        hovertext.GetComponent<RectTransform>().anchoredPosition = new Vector3(Input.mousePosition.x - Screen.width/2, Input.mousePosition.y - Screen.height/2 + yPos, 0);
+        //hovertext.GetComponent<RectTransform>().anchoredPosition = new Vector3(Input.mousePosition.x - Screen.width/2, Input.mousePosition.y - Screen.height/2 + yPos, 0);
     }
     public void showhovertext()
     {
-        hovertext.SetActive(true);
+        hovertext = GameObject.FindGameObjectWithTag("HoverText");
+
+        if (hovertext != null)
+        {
+            hovertext.transform.GetChild(0).gameObject.SetActive(true);
+            hovertext.GetComponentInChildren<TMP_Text>().text = transcript;
+        }
     }
 
     public void hidehovertext()
     {
-        hovertext.SetActive(false);
+        hovertext = GameObject.FindGameObjectWithTag("HoverText");
+        
+        if (hovertext != null)
+        {
+            //hovertext.GetComponentInChildren<TMP_Text>().text = "";
+            hovertext.transform.GetChild(0).gameObject.SetActive(false);
+            
+        }
     }
 }
